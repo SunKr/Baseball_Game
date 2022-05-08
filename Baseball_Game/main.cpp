de9f13e6f9ac5d607837bfd0b989d1d85c830ba9
Seonmin Lee <sun95@vt.edu>
2022-05-09 00:15:42 +0900

    Add project files.

---
+++ Baseball_Game/main.cpp
@@ -0,0 +1,85 @@
+//야구게임 만들기
+// 1~9 사이의 랜덤한 ㅣ;숫자 3개를 얻어온다. 단, 숫자는 중복되어서는 안된다.
+// 3개의 숫자는 * * * 의 형태로 출력되고 이 3개의 숫자를 맞추는 게임이다.
+// 사용자는 이 3개의 숫자를 맞출때까지 계쏙해서 3개씩 숫자를 입력한다.
+// 만약 맞춰야할 숫자가 7, 3, 8 일 경우
+// 사용자는 3개의 숫자를 계속 입력한다.
+// 입력: 1, 2, 4, 를 입력했을경우 1,2,4 맞춰야할 숫자중
+// 아무것도 없으므로 out 을 출력한다.
+// 입력 : 7, 5, 9를 입력했을 경우  7은 맞춰야할 숫자중 있고 위치도 같으므로
+// strike이다 5, 9는 없으므루 출력은 1strike 0 ball을 출력한다.
+// 입력 7 8 6을 입력했을 경우 7은 ㅣ1strike 8은 숫자는 있지만 위치가 다르므로 ball 이된다.
+// 1strike 1ball 을 출력한다.
+
+//이렇게 출력을 하고 입력을 받으면서 최종식으로 3개의 숫자를 자리까지 모두 일치하게
+
+// 입력하면 게임이 종료된다. 만약 입력받은 숫자중 3개중 한개라도 0이 있을경우 게임을 종료한다.
+
+
+#include <iostream>
+#include <time.h>
+
+
+using namespace std;
+
+
+
+int main()
+{
+	cout << " Base_Ball_Game" << endl;
+
+	// random number
+	srand((unsigned int)time(0));
+
+	int i1, i2, i3;
+	int array[10] = {};
+
+
+	while (true)
+	{
+		//1~9 randomg number
+		for (int i = 0; i < 9; ++i)
+		{
+			array[i] = i + 1;
+		}
+
+
+		//swap algorithm 
+		//shuffle algorithm
+		int iTemp, swap1, swap2;
+
+		for (int i = 0; i < 9; ++i)
+		{
+			i1 = rand() % 9 ;
+			i2 = rand() % 9 ;
+
+
+			iTemp = array[i1];
+			array[i1] = array[i2];
+			array[i2] = iTemp;
+		}
+
+		// pick 3 random number among 1~9
+		for (int i = 0; i < 3; ++i)
+		{
+			cout << array[i] << endl;
+		}
+
+
+		// 3 random number
+		//array[0];
+		//array[1];
+		//array[2];
+
+
+
+		// break for test
+		break;
+		
+
+
+	}
+
+
+	return 0;
+}
